0b8f5e5c865ed5f7d139c4967b9ae6b799fc9343
nousinnihal009 <nousinnihal2004@gmail.com>
2026-04-13 22:07:37 +0530

    Create index.ts

---
+++ src/app/components/analysis/index.ts
@@ -0,0 +1,4 @@
+export { ScoreGauge } from './ScoreGauge';
+export { CategoryScoreBar } from './CategoryScoreBar';
+export { IssueChip } from './IssueChip';
+export { SectionCard } from './SectionCard';
