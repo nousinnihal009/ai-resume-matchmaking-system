3705db0579a07405ac4af0f68e844626537a2f06
nousinnihal009 <nousinnihal2004@gmail.com>
2026-04-14 13:22:29 +0530

    Update index.ts

--- src/app/components/analysis/index.ts
+++ src/app/components/analysis/index.ts
@@ -2,3 +2,11 @@ export { ScoreGauge } from './ScoreGauge';
 export { CategoryScoreBar } from './CategoryScoreBar';
 export { IssueChip } from './IssueChip';
 export { SectionCard } from './SectionCard';
+export { KeywordGapRing } from './KeywordGapRing';
+export { KeywordCloud } from './KeywordCloud';
+export { BulletRewriteCard } from './BulletRewriteCard';
+export { TemplateShowcase } from './TemplateShowcase';
+export { TailoredSummaryCard } from './TailoredSummaryCard';
+export { GapAnalysisBar } from './GapAnalysisBar';
+export { TailoringResults } from './TailoringResults';
+
